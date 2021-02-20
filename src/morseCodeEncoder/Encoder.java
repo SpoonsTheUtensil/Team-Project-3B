78dce6a24eb84d9084bf2751219de803b5119162
Abbas Dargahi <adargahi97@gmail.com>
2021-02-20 13:45:06 -0600

    Created encoder
    Created two methods inside of the encoder class to convert alphabetical characters to morse code

--- src/morseCodeEncoder/Encoder.java
+++ src/morseCodeEncoder/Encoder.java
@@ -2,18 +2,31 @@ package morseCodeEncoder;
 
 public class Encoder {
 	
-	public static String encode(String toEncode) {
+	public static String encode(String toEncode, TreeNode root) {
+		TreeNode current = root;
+		String str = "";
+		String result = "";
+		char currLetter;
 		
-		StringBuffer result = new StringBuffer("");
-		
-		
-		
-		
-		
-		
+		for (int i = 0; i < toEncode.length(); i++) {
+			currLetter = toEncode.charAt(i);
+			result = treeContains(current, currLetter, str);
+		}
 		return result.toString();
 		
 	}
 	
-
+	public static String treeContains(TreeNode root, char letter, String str) {
+		if (root.data == null) {
+			return str;
+		} else {
+			if (root.left.data != null) {
+				return treeContains(root.left, letter, str);
+			}
+			if (root.right.data != null) {
+				return treeContains(root.right, letter, str);
+			}
+			return str;
+		}
+	}	
 }

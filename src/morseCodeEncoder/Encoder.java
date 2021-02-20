092fa5221d0b1099c6783ebd0b43e3898dfd812f
Abbas Dargahi <adargahi97@gmail.com>
2021-02-20 13:59:54 -0600

    Update variable names
    updated variable names and treeContains function

--- src/morseCodeEncoder/Encoder.java
+++ src/morseCodeEncoder/Encoder.java
@@ -2,31 +2,30 @@ package morseCodeEncoder;
 
 public class Encoder {
 	
-	public static String encode(String toEncode, TreeNode root) {
+	public static String encode(String inputToEncode, TreeNode root) {
 		TreeNode current = root;
-		String str = "";
+		String tempStr = "";
 		String result = "";
 		char currLetter;
 		
-		for (int i = 0; i < toEncode.length(); i++) {
-			currLetter = toEncode.charAt(i);
-			result = treeContains(current, currLetter, str);
+		for (int i = 0; i < inputToEncode.length(); i++) {
+			currLetter = inputToEncode.charAt(i);
+			result = treeContains(current, currLetter, tempStr);
 		}
-		return result.toString();
-		
+		return result;
 	}
 	
-	public static String treeContains(TreeNode root, char letter, String str) {
+	public static String treeContains(TreeNode root, char letter, String tempStr) {
 		if (root.data == null) {
-			return str;
+			return tempStr;
 		} else {
 			if (root.left.data != null) {
-				return treeContains(root.left, letter, str);
+				return treeContains(root.left, letter, tempStr + ".");
 			}
 			if (root.right.data != null) {
-				return treeContains(root.right, letter, str);
+				return treeContains(root.right, letter, tempStr + "-");
 			}
-			return str;
+			return tempStr;
 		}
 	}	
 }
